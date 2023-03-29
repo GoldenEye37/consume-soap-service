8964518108f7025077ea23dec8d25bb2edbc1bfa
LDhimba <Ldhimba@bancabc.co.zw>
2023-03-29 10:34:20 +0200

    project setup

---
+++ src/main/java/com/example/consumesoapservice/ConsumeSoapServiceExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.consumesoapservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumeSoapServiceExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumeSoapServiceExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/consumesoapservice/ConsumeSoapServiceExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.consumesoapservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumeSoapServiceExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
